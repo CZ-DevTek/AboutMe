9b7d210fa6e1998c2e8d436834dce2659a4bf8f0
Carlos García Pérez <carlosziok@gmail.com>
2024-02-12 23:47:46 +0300

    Added Gradient

---
+++ AboutMe/LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MainViewController.swift
+//  AboutMe
+//
+//  Created by Carlos Garcia Perez on 10/2/24.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    private let user = "User"
+    private let password = "Password"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                    let welcomeVC = segue.destination as? WelcomeViewController
+                    welcomeVC?.userName = user
+        }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password") {
+                    self.passwordTextField.text = ""
+                }
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotCredentialsTapped(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(withTitle: "Did you forget your user name?", andMessage: "Your name is \(user)")
+            : showAlert(withTitle: "Did you forget your password?", andMessage: "Your password is \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
--- AboutMe/WelcomeViewController.swift
+++ AboutMe/WelcomeViewController.swift
@@ -13,8 +13,34 @@ final class WelcomeViewController: UIViewController {
     
     var userName: String!
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 20/255,
+        blue: 0/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 255/255,
+        green: 105/255,
+        blue: 0/255,
+        alpha: 1
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         welcomeLabel.text = "Welcome, \(userName ?? "")!"
+    }
+}
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
