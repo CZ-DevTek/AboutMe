fb64c76fcae9e8eedd843b7abd2f7fd0e15a4d06
Carlos García Pérez <carlosziok@gmail.com>
2024-02-13 00:44:12 +0300

    Add User Files

---
+++ AboutMe/Model/UserModel.swift
@@ -0,0 +1,22 @@
+//
+//  UserModel.swift
+//  AboutMe
+//
+//  Created by Carlos Garcia Perez on 13/2/24.
+//
+
+struct User {
+    let userName: String
+    let password: String
+    let person: Person
+}
+struct Person {
+    let name: String
+    let surname: String
+    let studies: String
+    let position: String
+    let biography: String
+}
+
+    static func getUser() -> User {
+}
---
+++ AboutMe/ViewControllers/UserViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserViewController.swift
+//  AboutMe
+//
+//  Created by Carlos Garcia Perez on 13/2/24.
+//
+
+import UIKit
+
+final class UserViewController: UIViewController {
+    
+    private let user = User.getUser()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
